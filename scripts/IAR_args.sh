cafd416d00ccf515f1bbab56a5e27b4305b08b4c
Niklas Baier <nbaier@i13hpc59.cluster>
2024-11-05 00:59:42 +0100

    run_results

--- scripts/IAR_args.sh
+++ scripts/IAR_args.sh
@@ -4,11 +4,11 @@ CUDA_LAUNCH_BLOCKING=1 CUDA_VISIBLE_DEVICES=2 python3 ../whisper_main.py --datas
                    --model_id "distil-whisper/distil-large-v3" \
                    --version "vanilla" \
                    --environment "cluster" \
-                   --train_state "NT" \
+                   --train_state "T" \
                    --device "cuda" \
                    --task "transcribe" \
                    --developer_mode "N" \
                    --augmentation "N" \
                    --additional_tokens "N"\
-                   --run_notes "first run with complete logging"
+                   --run_notes "1000 steps max"
 
